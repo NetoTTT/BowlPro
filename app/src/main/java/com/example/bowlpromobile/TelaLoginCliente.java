8dd0c5db14ce0661f698fd0fa11dfc1d7da75ae1
NetoTTT <foque222@gmail.com>
2023-12-15 20:26:18 -0300

    3 commit

--- app/src/main/java/com/example/bowlpromobile/TelaLoginCliente.java
+++ app/src/main/java/com/example/bowlpromobile/TelaLoginCliente.java
@@ -4,16 +4,24 @@ package com.example.bowlpromobile;
 
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TelaLoginCliente extends AppCompatActivity {
-
+    Button tt;
+    View conteiner1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_cliente);
 
 
+    }
+
+    private void allComp(){
+        tt = findViewById(R.id.TT);
+        conteiner1 = findViewById(R.id.conteiner1);
     }
 }
