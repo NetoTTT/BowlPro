30cecc50da958a9e1c905bf908c828b5a57ddad7
NetoTTT <foque222@gmail.com>
2023-12-16 18:09:35 -0300

    5 commit

--- app/src/main/java/com/example/bowlpromobile/TelaLoginCliente.java
+++ app/src/main/java/com/example/bowlpromobile/TelaLoginCliente.java
@@ -4,20 +4,30 @@ package com.example.bowlpromobile;
 
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class TelaLoginCliente extends AppCompatActivity {
-    private EditText email,senha;
-    private Button bl;
-    private View conteiner1;
+    private EditText email_login,senha_login;
+    private Button bl,buttonEntrar;
+    private View conteiner1_login;
     private TextView cadastro;
+    String msg = "Preencha todos os campos!";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,14 +44,88 @@ public class TelaLoginCliente extends AppCompatActivity {
             }
         });
 
+        buttonEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = email_login.getText().toString();
+                String senha = senha_login.getText().toString();
+
+                if( email.isEmpty() || senha.isEmpty()){
+
+                    Snackbar snackbar = Snackbar.make(v, msg, Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+
+                } else {
+
+                    AuthCliente(v);
+
+                }
+
+            }
+        });
+
+    }
+
+    public void AuthCliente( View v){
+
+        String email = email_login.getText().toString();
+        String senha = senha_login.getText().toString();
+
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()){
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            Menu_Cliente();
+
+
+                        }
+                    }, 1000);
+                } else {
+                    String erro;
+
+                    try {
+                        throw task.getException();
+
+                    } catch (Exception e){
+
+                        erro = "Erro ao logar usuário!";
+                    }
+
+                    Snackbar snackbar = Snackbar.make(v, erro, Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+
+                }
+            }
+        });
+    }
+
+    private void Menu_Cliente(){
+
+        Intent c = new Intent(getApplicationContext(), Menu_Cliente.class);
+        startActivity(c);
+        finish();
     }
 
 
+
     private void allComp(){
-        conteiner1 = findViewById(R.id.conteiner1);
+        conteiner1_login = findViewById(R.id.conteiner1);
         bl = findViewById(R.id.bl);
-        email = findViewById(R.id.email_edit_login);
-        senha = findViewById(R.id.senha_edit_login);
+        email_login = findViewById(R.id.email_edit_login);
+        senha_login = findViewById(R.id.senha_edit_login);
         cadastro = findViewById(R.id.CadastrarCliente);
+        buttonEntrar = findViewById(R.id.buttonEntrar);
     }
 }
