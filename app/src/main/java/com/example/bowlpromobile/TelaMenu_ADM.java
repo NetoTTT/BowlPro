8b5b7ef17a25aa509f033badf3c2a2becc20b1c6
NetoTTT <foque222@gmail.com>
2023-12-17 19:43:21 -0300

    13 commit

---
+++ app/src/main/java/com/example/bowlpromobile/TelaMenu_ADM.java
@@ -0,0 +1,14 @@
+package com.example.bowlpromobile;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TelaMenu_ADM extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.agendar_horario);
+
+    }
+}
