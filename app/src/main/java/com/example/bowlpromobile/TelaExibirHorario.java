fa5403891d0c5cb582f3396895d45efd92db2661
NetoTTT <foque222@gmail.com>
2023-12-17 00:57:50 -0300

    8 commit

--- app/src/main/java/com/example/bowlpromobile/TelaExibirHorario.java
+++ app/src/main/java/com/example/bowlpromobile/TelaExibirHorario.java
@@ -3,22 +3,20 @@ package com.example.bowlpromobile;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class TelaAgendarHorario extends AppCompatActivity {
+public class TelaExibirHorario extends AppCompatActivity {
 
-    Button bl3;
-    ImageView voltarMenuCliente;
+    private ImageView voltarMenu2;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.agendar_horario);
-        allCompAgendarHorario();
-        bl3.setVisibility(View.INVISIBLE);
-        voltarMenuCliente.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.exibir_horario);
+        allCompExibirHorario();
+
+        voltarMenu2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), Menu_Cliente.class);
@@ -29,8 +27,7 @@ public class TelaAgendarHorario extends AppCompatActivity {
 
     }
 
-    private void allCompAgendarHorario(){
-        voltarMenuCliente = findViewById(R.id.iconVoltarforMenuCliente);
-        bl3 = findViewById(R.id.bl3);
+    private void allCompExibirHorario(){
+        voltarMenu2 = findViewById(R.id.iconVoltarforMenuCliente2);
     }
 }
--- app/src/main/java/com/example/bowlpromobile/TelaPerfil.java
+++ app/src/main/java/com/example/bowlpromobile/TelaPerfil.java
@@ -3,22 +3,19 @@ package com.example.bowlpromobile;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class TelaAgendarHorario extends AppCompatActivity {
+public class TelaPerfil extends AppCompatActivity {
 
-    Button bl3;
-    ImageView voltarMenuCliente;
-
+    private ImageView voltaMenuCliente3;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.agendar_horario);
-        allCompAgendarHorario();
-        bl3.setVisibility(View.INVISIBLE);
-        voltarMenuCliente.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.perfil);
+        allCompTelaPerfil();
+
+        voltaMenuCliente3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), Menu_Cliente.class);
@@ -26,11 +23,9 @@ public class TelaAgendarHorario extends AppCompatActivity {
                 finish();
             }
         });
-
     }
 
-    private void allCompAgendarHorario(){
-        voltarMenuCliente = findViewById(R.id.iconVoltarforMenuCliente);
-        bl3 = findViewById(R.id.bl3);
+    private void allCompTelaPerfil(){
+        voltaMenuCliente3 = findViewById(R.id.iconVoltarforMenuCliente3);
     }
 }
